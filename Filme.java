df891abe1a8fac501b2e63de77fe23d5d1b2e756
WalterNet0 <162221447+WalterNet0@users.noreply.github.com>
2024-08-11 23:14:22 -0300

    Delete Filme.java

--- Filme.java
+++
@@ -1,9 +0,0 @@
-package Eventos;
-
-public class Filme extends Evento{
-
-    public Filme(String nome, String data, String hora, String local, double preco_ingresso){
-        super(nome, data, hora, local, 200, preco_ingresso);
-    }
-
-}
