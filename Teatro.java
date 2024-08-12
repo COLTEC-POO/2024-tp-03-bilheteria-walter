d9d438552888bed5b8de098b345eb07a705675ef
WalterNet0 <162221447+WalterNet0@users.noreply.github.com>
2024-08-11 23:14:41 -0300

    Delete Teatro.java

--- Teatro.java
+++
@@ -1,9 +0,0 @@
-package Eventos;
-
-public class Teatro extends Evento{
-
-    public Teatro(String nome, String data, String hora, String local, double preco_ingresso){
-        super(nome, data, hora, local, 250, preco_ingresso);
-    }
-
-}
